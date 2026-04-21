c6b97abccb97b296ca1d5b016fdc98264933a893
Ana Clara <contato.anaclaralelis@gmail.com>
2026-04-21 16:59:59 -0300

    funções todas prontas do arquivo carrinho

--- src/carrinho.js
+++ src/carrinho.js
@@ -1,10 +1,74 @@
-function adicionarAoCarrinho(carrinho, produto, quantidade) {}
+function adicionarAoCarrinho(carrinho, produto, quantidade) {
+  if (!Array.isArray(carrinho)) {
+    throw new Error("Carrinho inválido");
+  }
 
-function removerDoCarrinho(carrinho, idProduto) {}
+  if (!produto || typeof produto !== "object") {
+    throw new Error("Produto inválido");
+  }
 
-function calcularSubtotal(carrinho) {}
+  if (quantidade <= 0) {
+    throw new Error("Quantidade deve ser maior que 0");
+  }
 
-// Exporta funções para uso em outros arquivos
+  if (produto.estoque < quantidade) {
+    throw new Error("Estoque insuficiente");
+  }
+
+  const item = carrinho.find(p => p.produto.id === produto.id);
+
+  if (item) {
+    item.quantidade += quantidade;
+  } else {
+    carrinho.push({
+      produto,
+      quantidade
+    });
+  }
+
+  produto.estoque -= quantidade;
+
+  return carrinho;
+}
+
+function removerDoCarrinho(carrinho, idProduto) {
+  if (!Array.isArray(carrinho)) {
+    throw new Error("Carrinho inválido");
+  }
+
+  const index = carrinho.findIndex(p => p.produto.id === idProduto);
+
+  if (index === -1) {
+    throw new Error("Produto não está no carrinho");
+  }
+
+  const itemRemovido = carrinho[index];
+
+  itemRemovido.produto.estoque += itemRemovido.quantidade;
+
+  carrinho.splice(index, 1);
+
+  return carrinho;
+}
+
+function calcularSubtotal(carrinho) {
+  if (!Array.isArray(carrinho)) {
+    throw new Error("Carrinho inválido");
+  }
+
+  if (carrinho.length === 0) {
+    return 0;
+  }
+
+  let total = 0;
+
+  carrinho.forEach(item => {
+    total += item.produto.preco * item.quantidade;
+  });
+
+  return total;
+}
+
 export {
   adicionarAoCarrinho,
   removerDoCarrinho,
